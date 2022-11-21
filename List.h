6d39976bd96a3a4ad020532234a23d94da7c42ed
Chen-Yuan-Lai <a186235@gmail.com>
2022-11-21 20:45:07 +0800

    new file main and add some function into .cpp file and header file

---
+++ List.h
@@ -0,0 +1,38 @@
+#ifndef LIST_H
+#define LIST_H
+using namespace std;
+template <class T>
+class List
+{ // A finite ordered list with zero or more elements.
+public:
+    List(int queueCapcity = 10);
+    // Create and empty queue whose initial capicity is queueCapicity
+
+    int IsEmpty() const;
+    // If number of element in the queue is 0, return 1 else return 0
+
+    T &Front() const;
+    // Return the element at the front og the queue
+
+    T &Rear() const;
+    // Return the element at the rear of the queue
+
+    void Push(const T &item);
+    // Insert item at rear of the queue
+
+    void Pop();
+    // Delete the front element of the queue
+
+    void changeSize(const int newSize);
+    // resizing the queue array
+
+    template <class U>
+    friend ostream &operator<<(ostream &os, List<U> &s);
+
+private:
+    T *queue;     // array for queue elements
+    int front,    // one counterclockwise from front
+        rear,     // array position of rear element
+        capacity; // capacity of queue array
+};
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "List.h"
+using namespace std;
+
+int main()
+{
+    Lsit<int> a(10);
+    cout << a << endl;
+}
